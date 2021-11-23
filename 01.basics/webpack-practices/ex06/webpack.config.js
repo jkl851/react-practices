effc31a0d52652493ea9d9566fd8f29858c73707
jkl851 <jkl851@naver.com>
2021-11-23 10:33:26 +0900

    webpack #finish

---
+++ 01.basics/webpack-practices/ex06/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const loader = require('sass-loader');
+module.exports = {
+    mode: 'development',
+    entry: path.resolve('src/index.js'), // 엔트리 파일 목록
+    output: {
+        path: path.resolve('public'), // 번들 파일 폴더
+        filename: 'bundle.js', //  '[name].bundle.js' 번들 파일 이름 규칙
+        assetModuleFilename: 'assets/images/[hash][ext]'
+    },
+    module: {
+        rules: [{
+            test: /\.(sa|sc|c)ss$/i, //   .(sa나 sc나 s)ss(공통) 로 끝나는($) 모든 파일
+            use: ['style-loader', 'css-loader', 'sass-loader']
+        }, {
+            test: /\.(png|gif|jpe?g|svg|ico|tiff?|bmp)$/i, //jpe?g   e가 있어도 되고, 없어도 되는 정규표현식
+            type: 'asset/resource'
+        }]
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        host: '0.0.0.0',
+        port: 9999,
+        //inline: true,
+        hot: false,
+        compress: true,
+        historyApiFallback: true
+    }
+}
