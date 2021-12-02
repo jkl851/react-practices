baa8871cacb50ed660d28c60eeee8f65db20d60f
jkl851 <jkl851@naver.com>
2021-12-02 11:57:13 +0900

    emaillist #2

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import './assets/scss/App.scss';
 import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+import './assets/scss/App.scss';
+
 
 export default function() {
     return (
         <div className={'App'}>
           <RegisterForm />
-          
+          <SearchBar />
+          <Emaillist />
         </div>
     )
 }
