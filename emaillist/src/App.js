edca36b471c977bb2962225d478a706b129e84dd
jkl851 <jkl851@naver.com>
2021-12-02 13:49:56 +0900

    emaillist #3

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterForm from './RegisterForm';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
+
 import './assets/scss/App.scss';
 
+import data from './assets/json/data.json';
 
 export default function() {
+    const [keyword, setKeyword] = useState('');
+
+    const notifyKeywordChanged = (keyword) => {
+      setKeyword(keyword);
+    };
+    
     return (
         <div className={'App'}>
           <RegisterForm />
-          <SearchBar />
-          <Emaillist />
+          <SearchBar keyword={keyword} callback={notifyKeywordChanged} />
+          <Emaillist keyword={keyword} emails={data}/>
         </div>
     )
 }
