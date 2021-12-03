bd4fecc65e13e60caeff9e9bbbe747fd6e774dac
jkl851 <jkl851@naver.com>
2021-12-03 15:47:06 +0900

    component lifecycle

--- guestbook/src/Guestbook.js
+++ guestbook/src/Guestbook.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import WriteForm from './WriteForm';
 import MessageList from './MessageList';
 import styles from './assets/scss/Guestbook.scss';
@@ -7,19 +7,29 @@ import data from './assets/json/data.json';
 
 export default function Guestbook() {
     const [messages, setMessages] = useState(data);
+
+    useEffect(() => {
+        console.log('최초 메세지 리스트 가져오기');
+        fetchMessageList();
+    }, []);
+
     const notifyMessage = {
         add: function(message) {
             console.log('ajax posting.....');
             // 성공했다고 가정
             message.no = 10;
             message.password = '';
-
+            console.log([message, ...messages]);
             setMessages([message, ...messages]);
         },
         delete: function(no) {
             setMessages(messages.filter(message => message.no !== no));
         }
     }
+
+    const fetchMessageList = () => {
+        console.log('message list 가져오기');
+    };
 
     return (
         <div className={styles.ScrollOuter}>
